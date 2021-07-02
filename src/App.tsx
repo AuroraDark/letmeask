4d924aa14063b2ece46eadc6104bab01acd32423
AuroraDark <andressa.workti@gmail.com>
2021-07-02 16:12:25 -0300

    commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Home } from './pages/Home';
+import { BrowserRouter, Route } from "react-router-dom";
+import { NewRoom } from './pages/NewRoom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Route path="/" exact component={Home}/>
+      <Route path="/rooms/new" component={NewRoom}/>
+    </BrowserRouter>
+  );
+}
+
+export default App;
